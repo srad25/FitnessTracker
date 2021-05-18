e4ddf4773c0806500cf89a4b173b6bf9752755ec
srad25 <suzanne.radlein@gmail.com>
2021-05-17 23:51:12 -0400

    adding routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = function(app) {
+
+    
+  
+    app.get("/exercise", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+  
+    app.get("/stats", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "..public/index.html"));
+    })
+  
+  };
--- server.js
+++ server.js
@@ -16,7 +16,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
