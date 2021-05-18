1776db2296eb1586366de08839048c6ec32723fc
srad25 <suzanne.radlein@gmail.com>
2021-05-18 06:24:00 -0400

    re-wrote code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,51 +2,46 @@ const db = require("../models");
 const router = require("express").Router();
 
 router.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-      .sort({ day: -1 })
-      .limit(1)
-      .then((dbWorkout) => {
-        res.json(dbWorkout);
-      })
-      .catch((err) => {
-        res.status(400).json(err);
-      });
-  });
-  
-
-router.post("/api/workouts", (req, res) => {
-  db.Workout.create(req.body, (error, data) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.send(data);
-    }
-  });
-});
-
-router.put("/api/workouts/:id", (req, res) => {
-  db.Workout.updateOne(
-    { _id: req.params.id },
-    { $push: { exercises: req.body } },
-    (error, edited) => {
-      if (error) {
-        console.log(error);
-        res.send(error);
-      } else {
-        console.log(edited);
-        res.send(edited);
-      }
-    }
-  );
-});
-
-router.get("/api/workouts/range", (req, res) => {
   db.Workout.find({})
+    .sort({ day: -1 })
+    .limit(1)
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
     .catch((err) => {
-      res.json(err);
+      res.status(400).json(err);
+    });
+});
+
+
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create(req.body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.updateOne(
+    {_id: req.params.id}, { $push: {exercises: req.body}})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", ({}, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
     });
 });
 
